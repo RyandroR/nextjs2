ec4e262fc365e349838f624d372ab1d3f5c4158b
RyandroR <rubbish.lap@gmail.com>
2022-09-08 14:34:57 +0700

    benerin morning

--- pages/morning.js
+++ pages/morning.js
@@ -1,22 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-
+import {server} from '../config'
 
 export default function Home({mats}) {
   return (
     <div className={styles.container}>
      <div className={styles.grid}>
       {mats?.map((mats) => (
-        <div key={mats.id}>
-            <Link href={`/data/${mats.id}`}>
-              <a className={styles.card}>
+         <Link href={`/data/${mats.id}`}>
+            <a className={styles.card}>
               <h3>{mats.title} &rarr;</h3>
-             <p>{mats.body}</p>
-              </a>
-            </Link>
-        </div>
+              <p>{mats.body}</p>
+            </a>
+          </Link>
       ))}
       </div>
     </div>
